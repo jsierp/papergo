8924e04ea367b80b16c82b543d8c64d09c78cb46
Jakub Czyz <me@jakubc.pl>
2025-07-26 21:35:31 +0200

    fix rendering and timing

--- renderer.go
+++ renderer.go
@@ -4,7 +4,8 @@ import "fmt"
 
 const (
 	cursorTo        = "\033[%d;%dH"
-	colorBlue       = "\033[34m"
+	colorBlue       = "\033[44m"
+	colorRed        = "\033[41m"
 	colorReset      = "\033[0m"
 	clearScreen     = "\033[2J"   // Clears the entire screen
 	cursorHome      = "\033[H"    // Moves cursor to home position (1,1)
@@ -12,15 +13,13 @@ const (
 	showCursor      = "\033[?25h" // Shows the cursor
 	alternateScreen = "\033[?1049h"
 	mainScreen      = "\033[?1049l"
-	EmptySquare     = '⬜'
-	FilledSquare    = '⬛'
 )
 
 type Renderer struct {
 	World [][]uint8
 }
 
-func NewRendrer(world [][]uint8) *Renderer {
+func NewRenderer(world [][]uint8) *Renderer {
 	fmt.Print(alternateScreen)
 	fmt.Print(hideCursor)
 
@@ -32,7 +31,7 @@ func (r *Renderer) Destroy() {
 	fmt.Print(showCursor)
 }
 
-func (r *Renderer) Render(row int, col int, char rune) {
+func (r *Renderer) Render(row int, col int, color string) {
 	fmt.Printf(cursorTo, row+1, col*2+1)
-	fmt.Printf("%s%c", colorBlue, char)
+	fmt.Printf("%s  ", color)
 }
